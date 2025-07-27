b228f5ed8dd46e8c2dbb850a174b7e38545a4b0d
m <mkdltorre@gmail.com>
2025-07-27 18:27:32 +0800

    style: format main

--- optimizon_de_test/__main__.py
+++ optimizon_de_test/__main__.py
@@ -1,8 +1,16 @@
 import config
 from optimizon_de_test.gcs_uploader import stream_upload_to_gcs
 
-stream_upload_to_gcs(
-    csv_url="https://storage.googleapis.com/nozzle-csv-exports/testing-data/order_items_data_1_.csv",
-    bucket_name=config.GCS_BUCKET,
-    blob_name="streaming_test.csv"
-)
+CSV_URL = "https://storage.googleapis.com/nozzle-csv-exports/testing-data/" \
+    "order_items_data_1_.csv"
+
+def main():
+    print('Starting pipeline...')
+    stream_upload_to_gcs(
+        csv_url=CSV_URL,
+        bucket_name=config.GCS_BUCKET,
+        blob_name="streaming_test.csv"
+    )
+
+if __name__ == "__main__":
+    main()
