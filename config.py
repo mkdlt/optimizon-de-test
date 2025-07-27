5a320efa6bb503af9b991ee7b76f70db87316e39
m <mkdltorre@gmail.com>
2025-07-27 16:26:17 +0800

    chore: add config file

---
+++ config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GCP_PROJECT = os.getenv("GCP_PROJECT")
+GCS_BUCKET = os.getenv("GCS_BUCKET")
+BQ_DATASET = os.getenv("BQ_DATASET")
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
