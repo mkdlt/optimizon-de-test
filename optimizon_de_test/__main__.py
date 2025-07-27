0f4805efcd274480243e11e33dbf4fff3dcef45d
m <mkdltorre@gmail.com>
2025-07-27 21:43:02 +0800

    feat: add gcs into bq load

--- optimizon_de_test/__main__.py
+++ optimizon_de_test/__main__.py
@@ -1,5 +1,6 @@
 import config
 from optimizon_de_test.gcs_uploader import stream_upload_to_gcs
+from optimizon_de_test.bigquery_loader import load_csv_to_bigquery
 
 CSV_URL = (
     "https://storage.googleapis.com/nozzle-csv-exports/testing-data/"
@@ -8,10 +9,20 @@ CSV_URL = (
 
 def main():
     print('Starting pipeline...')
-    stream_upload_to_gcs(
+
+    # gcs_uri = 'gs://optimizon-de-test-data/streaming_test.csv'
+    
+    gcs_uri = stream_upload_to_gcs(
         csv_url=CSV_URL,
         bucket_name=config.GCS_BUCKET,
         blob_name="streaming_test.csv"
+    )
+
+    table_id=f"{config.GCP_PROJECT}.{config.BQ_DATASET}.loading_test"
+
+    load_csv_to_bigquery(
+        gcs_uri=gcs_uri,
+        table_id=table_id
     )
 
 if __name__ == "__main__":
---
+++ optimizon_de_test/bigquery_loader.py
@@ -0,0 +1,45 @@
+from google.cloud import bigquery
+
+def load_csv_to_bigquery(gcs_uri: str, table_id: str):
+    client = bigquery.Client()
+
+    schema = [
+        bigquery.SchemaField("order_id", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("purchased_at", "STRING"),
+        bigquery.SchemaField("purchased_date", "STRING"),
+        bigquery.SchemaField("purchased_month_ended", "STRING"),
+        bigquery.SchemaField("order_item_id", "STRING"),
+        bigquery.SchemaField("sku", "STRING"),
+        bigquery.SchemaField("product_title", "STRING"),
+        bigquery.SchemaField("product_name_full", "STRING"),
+        bigquery.SchemaField("currency", "STRING"),
+        bigquery.SchemaField("item_price", "STRING"),
+        bigquery.SchemaField("item_tax", "STRING"),
+        bigquery.SchemaField("shipping_price", "STRING"),
+        bigquery.SchemaField("shipping_tax", "STRING"),
+        bigquery.SchemaField("gift_wrap_price", "STRING"),
+        bigquery.SchemaField("gift_wrap_tax", "STRING"),
+        bigquery.SchemaField("item_promo_discount", "STRING"),
+        bigquery.SchemaField("shipment_promo_discount", "STRING"),
+        bigquery.SchemaField("ship_service_level", "STRING"),
+    ]
+
+    job_config = bigquery.LoadJobConfig(
+        schema=schema,
+        source_format=bigquery.SourceFormat.CSV,
+        skip_leading_rows=1,
+        allow_quoted_newlines=True,
+        max_bad_records=1000
+    )
+
+    print(f"Loading {gcs_uri} into {table_id}")
+
+    load_job = client.load_table_from_uri(
+        gcs_uri, table_id, job_config=job_config
+    )
+
+    load_job.result()
+
+    destination_table = client.get_table(table_id)
+    print(f"Loaded {destination_table.num_rows} rows")
+
