6d8731834cc4b631d0b88d3b0af8a0066583db81
m <mkdltorre@gmail.com>
2025-07-28 15:34:33 +0800

    style: fix debug prints

--- optimizon_de_test/bigquery_loader.py
+++ optimizon_de_test/bigquery_loader.py
@@ -32,7 +32,7 @@ def load_csv_to_raw_layer(gcs_uri: str, raw_table_id: str):
         max_bad_records=1000
     )
 
-    print(f"Loading {gcs_uri} into {raw_table_id}")
+    print(f"Loading raw data into {raw_table_id}")
 
     job = client.load_table_from_uri(
         gcs_uri, raw_table_id, job_config=job_config
@@ -40,13 +40,13 @@ def load_csv_to_raw_layer(gcs_uri: str, raw_table_id: str):
 
     job.result()
 
-    destination_table = client.get_table(raw_table_id)
+    raw_table = client.get_table(raw_table_id)
 
     load_time = job.ended - job.started
-    num_rows = destination_table.num_rows
+    num_rows = raw_table.num_rows
     
-    print(f'Loaded {destination_table.num_rows} rows')
-    print(f'Loading time: {load_time}')
+    print(f'Loaded {raw_table.num_rows} rows')
+    print(f'Load time: {load_time}')
 
     return load_time, num_rows
 
